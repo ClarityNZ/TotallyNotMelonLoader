c4189c507ae357a7d50afe6ecbb242f01d6f1d8b
ClarityNZ <50315458+ClarityNZ@users.noreply.github.com>
2020-12-18 04:02:58 +1300

    Add files via upload

---
+++ MyMod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+
+namespace TotallyNotMelonLoader
+{
+    public class MyMod : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Log("Mod has been loaded, console title will be changed in a moment...");
+            MelonPrefs.RegisterCategory("TotallyNotMelonLoader", "TotallyNotMelonLoader");
+            MelonPrefs.RegisterString("TotallyNotMelonLoader", "Desired Name", " ");
+
+            Console.Title = MelonPrefs.GetString("TotallyNotMelonLoader", "Desired Name");
+
+        }
+    }
+}
